2a5f980a1e75868f3b575879a0521e5b692fd4fc
roel.vercammen <roel.vercammen@b60b181c-f2dd-11dd-88c7-29a3b14d5316>
2009-02-23 17:42:28 +0000

    Implementatie getters en setters in Meetgegevens view
    git-svn-id: https://audioadmin.googlecode.com/svn/trunk@38 b60b181c-f2dd-11dd-88c7-29a3b14d5316

--- AudioAdmin/src/view/view_meetgegevens.cpp
+++ AudioAdmin/src/view/view_meetgegevens.cpp
@@ -13,3 +13,147 @@ Meetgegevens::Meetgegevens(::QWidget *parent)
 Meetgegevens::~Meetgegevens()
 {
 }
+
+int Meetgegevens::getLGRechtsData(int Hz)
+{
+    Q_ASSERT(m_tonaleAudiometrieRechts);
+    return m_tonaleAudiometrieRechts->getLGdata(Hz);
+}
+
+int Meetgegevens::getBGRechtsData(int Hz)
+{
+    Q_ASSERT(m_tonaleAudiometrieRechts);
+    return m_tonaleAudiometrieRechts->getBGdata(Hz);
+}
+
+int Meetgegevens::getUCLRechtsData(int Hz)
+{
+    Q_ASSERT(m_tonaleAudiometrieRechts);
+    return m_tonaleAudiometrieRechts->getUCLdata(Hz);
+}
+
+int Meetgegevens::getLGLinksData(int Hz)
+{
+    Q_ASSERT(m_tonaleAudiometrieLinks);
+    return m_tonaleAudiometrieLinks->getLGdata(Hz);
+}
+
+int Meetgegevens::getBGLinksData(int Hz)
+{
+    Q_ASSERT(m_tonaleAudiometrieLinks);
+    return m_tonaleAudiometrieLinks->getBGdata(Hz);
+}
+
+int Meetgegevens::getUCLLinksData(int Hz)
+{
+    Q_ASSERT(m_tonaleAudiometrieLinks);
+    return m_tonaleAudiometrieLinks->getUCLdata(Hz);
+}
+
+int Meetgegevens::getROZonderData(int dB)
+{
+    Q_ASSERT(m_vocaleAudiometrieZonder);
+    return m_vocaleAudiometrieZonder->getROdata(dB);
+}
+
+int Meetgegevens::getLOZonderData(int dB)
+{
+    Q_ASSERT(m_vocaleAudiometrieZonder);
+    return m_vocaleAudiometrieZonder->getLOdata(dB);
+}
+
+int Meetgegevens::getROLOZonderData(int dB)
+{
+    Q_ASSERT(m_vocaleAudiometrieZonder);
+    return m_vocaleAudiometrieZonder->getROLOdata(dB);
+}
+
+int Meetgegevens::getROMetData(int dB)
+{
+    Q_ASSERT(m_vocaleAudiometrieMet);
+    return m_vocaleAudiometrieMet->getROdata(dB);
+}
+
+int Meetgegevens::getLOMetData(int dB)
+{
+    Q_ASSERT(m_vocaleAudiometrieMet);
+    return m_vocaleAudiometrieMet->getLOdata(dB);
+}
+
+int Meetgegevens::getROLOMetData(int dB)
+{
+    Q_ASSERT(m_vocaleAudiometrieMet);
+    return m_vocaleAudiometrieMet->getROLOdata(dB);
+}
+
+void Meetgegevens::setLGRechtsData(int Hz, int dB)
+{
+    Q_ASSERT(m_tonaleAudiometrieRechts);
+    m_tonaleAudiometrieRechts->setLGdata(Hz, dB);
+}
+
+void Meetgegevens::setBGRechtsData(int Hz, int dB)
+{
+    Q_ASSERT(m_tonaleAudiometrieRechts);
+    m_tonaleAudiometrieRechts->setBGdata(Hz, dB);
+}
+
+void Meetgegevens::setUCLRechtsData(int Hz, int dB)
+{
+    Q_ASSERT(m_tonaleAudiometrieRechts);
+    m_tonaleAudiometrieRechts->setUCLdata(Hz, dB);
+}
+
+void Meetgegevens::setLGLinksData(int Hz, int dB)
+{
+    Q_ASSERT(m_tonaleAudiometrieLinks);
+    m_tonaleAudiometrieLinks->setLGdata(Hz, dB);
+}
+
+void Meetgegevens::setBGLinksData(int Hz, int dB)
+{
+    Q_ASSERT(m_tonaleAudiometrieLinks);
+    m_tonaleAudiometrieLinks->setBGdata(Hz, dB);
+}
+
+void Meetgegevens::setUCLLinksData(int Hz, int dB)
+{
+    Q_ASSERT(m_tonaleAudiometrieLinks);
+    m_tonaleAudiometrieLinks->setUCLdata(Hz, dB);
+}
+
+void Meetgegevens::setROZonderData(int dB, int percentage)
+{
+    Q_ASSERT(m_vocaleAudiometrieZonder);
+    m_vocaleAudiometrieZonder->setROdata(dB, percentage);
+}
+
+void Meetgegevens::setLOZonderData(int dB, int percentage)
+{
+    Q_ASSERT(m_vocaleAudiometrieZonder);
+    m_vocaleAudiometrieZonder->setLOdata(dB, percentage);
+}
+
+void Meetgegevens::setROLOZonderData(int dB, int percentage)
+{
+    Q_ASSERT(m_vocaleAudiometrieZonder);
+    m_vocaleAudiometrieZonder->setROLOdata(dB, percentage);
+}
+
+void Meetgegevens::setROMetData(int dB, int percentage)
+{
+    Q_ASSERT(m_vocaleAudiometrieMet);
+    m_vocaleAudiometrieMet->setROdata(dB, percentage);
+}
+
+void Meetgegevens::setLOMetData(int dB, int percentage)
+{
+    Q_ASSERT(m_vocaleAudiometrieMet);
+    m_vocaleAudiometrieMet->setLOdata(dB, percentage);
+}
+
+void Meetgegevens::setROLOMetData(int dB, int percentage)
+{
+    Q_ASSERT(m_vocaleAudiometrieMet);
+    m_vocaleAudiometrieMet->setROLOdata(dB, percentage);
+}
